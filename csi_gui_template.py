53e21e8e7561cf2da43ef0c14dcf20290414a987
Cyber Secrets - Red Team github <infosecwriter@gmail.com>
2023-06-06 17:28:37 -0600

    Add files via upload
    updated copyright for the template

--- csi_gui_template.py
+++ csi_gui_template.py
@@ -1,3 +1,23 @@
+#! /usr/bin/python3
+# ----------------------------------------------------------------------------
+# CSI Linux (https://www.csilinux.com)
+# Author: Author names here
+# Copyright (C) CSI Linux. All rights reserved.
+#
+# This software is proprietary and NOT open source. Redistribution,
+# modification, or any other use of this code is strictly prohibited without
+# the express written consent of CSI Linux.
+#
+# This software is provided "AS IS" without warranty of any kind, express or
+# implied, including but not limited to the warranties of merchantability,
+# fitness for a particular purpose, and non-infringement. In no event shall
+# the authors or copyright holders be liable for any claim, damages, or other
+# liability, whether in an action of contract, tort or otherwise, arising from,
+# out of or in connection with the software or the use or other dealings in
+# the software.
+#
+# Paid support can be contracted through support@csilinux.com
+# ----------------------------------------------------------------------------
 import time
 import webbrowser
 import requests
